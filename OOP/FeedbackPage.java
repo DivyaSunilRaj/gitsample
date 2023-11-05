1a6635ceb1c7c2fec61c110e128f2663932d2311
DivyaSunilRaj <148881745+DivyaSunilRaj@users.noreply.github.com>
2023-11-05 10:46:23 +0000

    first change

--- OOP/FeedbackPage.java
+++ OOP/FeedbackPage.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FeedbackPage {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+ {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
         System.out.println("Please enter your feedback:");
+
         String feedback = reader.readLine();
+
         System.out.println("Thank you for your feedback: " + feedback);
     }
 }
